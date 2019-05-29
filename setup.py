5625cec4781ca0fdbbbbcf2923b55c9efaea69de
Erick <lourenco.erick@gmail.com>
2019-05-29 19:13:31 -0300

    first commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='toolboxds',
+      version='0.1',
+      description='The funniest joke in the world',
+      url='http://github.com/storborg/funniest',
+      author='Flying Circus',
+      author_email='flyingcircus@example.com',
+      license='MIT',
+      packages=['toolboxds'],
+      zip_safe=False)
