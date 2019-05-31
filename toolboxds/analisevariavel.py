be37409fbd0623d2957e5595c1cfef9c18189987
Erick <lourenco.erick@gmail.com>
2019-05-31 17:53:33 -0300

    analise cat plotly

--- toolboxds/analisevariavel.py
+++ toolboxds/analisevariavel.py
@@ -372,3 +372,117 @@ def analise_cat_1(df, var, y, label='', fl_ordena=0, num=False, q=0, q2=0):
         print(s.IV.sum())
         return (var, s.IV.sum())
         print('\n')
+
+
+def plotly_analise_cat_1(df, var, y, label='', fl_ordena=0, num=False, q=0, q2=0):
+
+    if label == '':
+        label = var
+    df_ = df.loc[:, [var, y]].copy()
+    if num:
+        if q == 0:
+            if q2 == 0:
+                df_[var+'_cat'] = pd.qcut(df_[var], q=[0.0, 0.2, 0.5, 0.8, 1.0],
+                                          retbins=True, duplicates='drop')[0].cat.add_categories('missing')
+                var = var+'_cat'
+            else:
+                df_[var+'_cat'] = pd.qcut(df_[var], q=q2, retbins=True,
+                                          duplicates='drop')[0].cat.add_categories('missing')
+                var = var+'_cat'
+        else:
+            df_[var+'_cat'] = pd.cut(df_[var], bins=q).cat.add_categories('missing')
+            var = var+'_cat'
+    print('\n')
+    print('Análise da variável {}'.format(var))
+    print('# valores distintos {}'.format(df_[var].nunique()))
+    try:
+        df_[var] = df_[var].fillna('missing')
+    except:
+        df_[var] = df_[var].cat.add_categories('missing').fillna('missing')
+
+    total_maus = df_[y].sum()
+    total_bons = df_.shape[0] - df_[y].sum()
+
+    def tx(x):
+        return sum(x)*100/sum(1 - x + x)
+
+    def pct(x):
+        return sum(1-x+x)*1.0 / (df_.shape[0])
+
+    def pct_maus(x):
+        return (sum(x)/total_maus)
+
+    def rr(x):
+        return (sum(x)/total_maus)/(sum(1-x)/total_bons)
+
+    def WoE(x):
+        return np.log(rr(x))
+
+    def IV(x):
+        return -(sum(1-x)/total_bons - sum(x)/total_maus)*WoE(x)
+    s = df_.groupby(var).agg({y: [np.size, pct, pct_maus, tx, rr, WoE, IV]})[y]
+
+    def color_negative_green(val):
+        color = 'green' if val < 0 else 'black'
+        return 'color: %s' % color
+    print('IV : {:.3f}'.format(s.IV.sum()))
+    #print('Beta : {:.3f}'.format(dic_beta[var]))
+    t = s.style.applymap(color_negative_green)
+    display(pd.DataFrame(df_[var].value_counts()).join(pd.DataFrame(df_[var].value_counts(
+        normalize=True)*100), lsuffix='k').rename(columns={var+'k': '#', var: '%'}))
+    display(t)
+    by_var = df_.groupby(var).agg({y: [np.size, np.mean]})[y]
+    if fl_ordena == 1:
+        by_var = by_var.sort_values(by='mean')
+    Y1 = by_var['size']
+    Y2 = by_var['mean']*100
+    Y_mean = np.ones(shape=(len(Y1.index))) * df_[y].mean()*100
+    index = [i for i in np.arange(len(Y1.index))]
+
+    trace1 = go.Bar(
+        x=index,
+        y=Y1,
+        name='# registros'
+    )
+    trace2 = go.Scatter(
+        x=index,
+        y=Y2,
+        name='tx. média evento',
+        yaxis='y2'
+    )
+
+    trace3 = go.Scatter(
+        x=index,
+        y=Y_mean,
+        name='tx. evento',
+        yaxis='y2'
+    )
+    data = [trace1, trace2, trace3]
+    layout = go.Layout(
+        title='Bivariada  {}'.format(label),
+        yaxis=dict(
+            title='# registros'
+        ),
+        xaxis=dict(
+            # title=by_var,
+            tickvals=index,
+            ticktext=[str(label) for label in Y2.index.tolist()]
+        ),
+        yaxis2=dict(
+            title='% evento',
+            titlefont=dict(
+                color='rgb(148, 103, 189)'
+            ),
+            tickfont=dict(
+                color='rgb(148, 103, 189)'
+            ),
+            overlaying='y',
+            side='right'
+        )
+    )
+    fig = go.Figure(data=data, layout=layout)
+    iplot(fig, filename='multiple-axes-double')
+
+    print(s.IV.sum())
+    return (var, s.IV.sum())
+    print('\n')
